f892356c32205fac8fb339547b7092b15c502139
Pietro Ivan Cerrato <ivan@cerrato.biz>
2020-11-09 17:09:24 +0100

    force camera hal1 for instagram, whatsapp and skype
    Author:    Pietro Ivan Cerrato <ivan@cerrato.biz>
    Date:      Mon Nov 9 17:09:24 2020 +0100

--- vendor-props.mk
+++ vendor-props.mk
@@ -47,4 +47,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.charger.enable_suspend=true \
     ro.hwui.use_vulkan=true \
     ro.hwui.render_ahead=1 \
+    ro.charger.enable_suspend=true \
+    vendor.camera.hal1.packagelist=com.instagram.android,com.whatsapp,com.skype.raider
 
